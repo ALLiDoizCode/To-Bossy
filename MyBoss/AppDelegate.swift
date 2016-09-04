9fe61e6a1083afb665f4d22065a70092e06fe6da
Jonathan <jonathan@Jonathans-iMac.fios-router.home>
2016-09-04 04:17:50 -0400

    WIP

--- MyBoss/AppDelegate.swift
+++ MyBoss/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: .Fade) // with animation option.
+        
         IQKeyboardManager.sharedManager().enable = true
         
         return true
--- MyBoss/Controller/RatingPage1ViewController.swift
+++ MyBoss/Controller/RatingPage1ViewController.swift
@@ -13,15 +13,19 @@ import Cartography
 
 class RatingPage1ViewController: UIViewController {
     
+    var titleView:MaterialView = MaterialView()
     var myTitle:MaterialLabel = MaterialLabel()
     var rateValueLabel: MaterialLabel! = MaterialLabel()
     var emojiRateView: EmojiRateView! = EmojiRateView()
     var next:FlatButton = FlatButton()
+    var back:FlatButton = FlatButton()
     
     let ratingTexts = ["Very bad", "Bad", "Normal", "Good", "Very good", "Perfect"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //myTitle.backgroundColor = MaterialColor.brown.darken1
         
         emojiRateView.rateValueChangeCallback = {(rateValue: Float) -> Void in
             self.rateValueLabel.text = String(
@@ -37,16 +41,22 @@ class RatingPage1ViewController: UIViewController {
         //rateValueLabel.backgroundColor = UIColor.blueColor()
         
         myTitle.text = "How was work today?"
+        myTitle.numberOfLines = 0
         myTitle.textColor = MaterialColor.cyan.darken1
         myTitle.textAlignment = .Center
-        myTitle.font = RobotoFont.regularWithSize(24)
+        myTitle.font = RobotoFont.regularWithSize(48)
+        
+        back.setTitle("Back", forState: .Normal)
+        back.setTitleColor(MaterialColor.cyan.darken1, forState: .Normal)
         
         self.view.addSubview(rateValueLabel)
         self.view.addSubview(emojiRateView)
         self.view.addSubview(next)
-        self.view.addSubview(myTitle)
+        self.view.addSubview(back)
+        self.view.addSubview(titleView)
+        self.titleView.addSubview(myTitle)
         
-        let views = [emojiRateView,rateValueLabel,next,myTitle]
+        let views = [emojiRateView,rateValueLabel,next,myTitle,back,titleView]
         
         constrain(views) { _views in
             
@@ -62,18 +72,36 @@ class RatingPage1ViewController: UIViewController {
             _views[1].right == (superView?.right)! - 20
             _views[1].height == 50
             
+            _views[2].centerX == (superView?.centerX)!
             _views[2].bottom ==  (superView?.bottom)! - 50
             _views[2].left == (superView?.left)! + 50
             _views[2].right ==  (superView?.right)! - 50
             
-            _views[3].bottom == _views[1].top - 20
-            _views[3].left == (superView?.left)! + 20
-            _views[3].right == (superView?.right)! - 20
-            _views[3].height == 30
+            _views[3].center == (_views[3].superview?.center)!
+            _views[3].width == 300
+            _views[3].height == 130
+            
+            //backBtn
+            _views[4].left == (superView?.left)!
+            _views[4].top == (superView?.top)! + 20
+            _views[4].width == 75
+            _views[4].height == 30
+            
+            //titleView
+            _views[5].size == superView!.size
+            _views[5].center == (_views[5].superview?.center)!
             
         }
         
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        /* UIView.animateWithDuration(3) {
+         
+         self.titleView.layer.opacity = 0
+         }*/
     }
 
     override func didReceiveMemoryWarning() {
